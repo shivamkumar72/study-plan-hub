2f0f9315d964fea0f5229448fcee1c1959f28ed3
Shivam Kumar <sp4833860@gmail.com>
2026-04-25 04:09:13 +0530

    fix: update to production settings for Render deployment

--- server/dist/utils/password.util.js
+++ server/dist/utils/password.util.js
@@ -4,13 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.comparePassword = exports.hashPassword = void 0;
-const bcrypt_1 = __importDefault(require("bcrypt"));
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
 const SALT_ROUNDS = 12;
 const hashPassword = async (plainText) => {
-    return bcrypt_1.default.hash(plainText, SALT_ROUNDS);
+    return bcryptjs_1.default.hash(plainText, SALT_ROUNDS);
 };
 exports.hashPassword = hashPassword;
 const comparePassword = async (plainText, hashedPassword) => {
-    return bcrypt_1.default.compare(plainText, hashedPassword);
+    return bcryptjs_1.default.compare(plainText, hashedPassword);
 };
 exports.comparePassword = comparePassword;
